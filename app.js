db572ab9678a0062d60700ecc56c12e0decc9700
Julie Branyan <juliebranyan@gmail.com>
2022-08-12 16:22:36 -0700

    sequelize setup

--- app.js
+++ app.js
@@ -10,6 +10,23 @@ const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'tr
 // create the Express app
 const app = express();
 
+//Setup Sequelize
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'fsjstd-restapi.db'
+});
+
+(async () => {
+  await sequelize.sync();
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database successful!');
+  } catch (error) {
+    console.error('Error connecting to the database: ', error);
+  }
+})();
+
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
 
