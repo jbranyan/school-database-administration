83c94025ef829c6ae7192b2efc6d0316a128c4f9
Julie Branyan <juliebranyan@gmail.com>
2022-08-17 08:01:12 -0700

    Add models for course and users

---
+++ models/user.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+var bcrypt = require('bcryptjs');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    userId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    firstName : {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Please enter a value for Title',
+        },
+        notNull: {
+          msg: 'Please provide a value for Title',
+        },
+      },
+    },
+    lastName : {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Please enter a value for Author',
+        },
+        notNull: {
+          msg: 'Please provide a value for Author',
+        },
+      },
+    },
+    emailAddress: {
+      type: DataTypes.STRING,
+      unique: {
+        msg: 'The email address entered already exists',
+      },
+      allowNull: false,
+      validate:{
+        notNull: {
+          msg: 'An email address is required',
+        },
+        isEmail: {
+          msg: 'Please provide a valid email address',
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A password is required',
+        },
+          notEmpty: {
+            msg: 'Please provide a password',
+          }
+      },
+      len: {
+        args: [10, 30],
+        msg: 'The password length should be between 10 and 30 characters'
+      },
+    },
+    confirmedPassword: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(val) {
+        if ( val === this.password ) {
+          const hashedPassword = bcrypt.hashSync(val, 10);
+          this.setDataValue('confirmedPassword', hashedPassword);
+        }
+      },
+      validate: {
+        notNull: {
+          msg: 'Passwords don\'t match'
+        }
+      }
+    }
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+    User.hasMany(models.Course, {foreignKey: 'userId'});
+  };
+  return User;
+};
