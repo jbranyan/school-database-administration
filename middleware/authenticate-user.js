7b2a1fd50e8a60b468aaffdd481cec080536c677
Julie Branyan <juliebranyan@gmail.com>
2022-08-24 21:15:52 -0700

    Authentication error messaging and try/catch

--- middleware/authenticate-user.js
+++ middleware/authenticate-user.js
@@ -6,24 +6,37 @@ const bcrypt = require('bcryptjs');
 
 exports.authenticateUser = async (req, res, next) => {
     let message;
-    const credentials = auth(req);
 
-    if(credentials){
-        const user = await User.findOne({where: {emailAddress: credentials.name}});
-        if(user){
-            const authenticated = bcrypt.compareSync(credentials.pass, user.password);
-            if(authenticated){
-                console.log(`User successfully authenticated ${user.email}`);
-                req.currentUser = user;
+    try {
+        const credentials = auth(req);
+
+        if(credentials){
+            const user = await User.findOne({
+                where: { emailAddress: credentials.name }
+            });
+            if(user){
+                const authenticated = bcrypt
+                .compareSync(credentials.pass, user.password);
+                if(authenticated){
+                    console.log(`User successfully authenticated ${user.firstName} ${user.lastName}`);
+                    req.currentUser = user;
+                } else {
+                    message = 'Authentication failure';
+                }
+            } else {
+                message = "user not found";
             }
         } else {
-            message = 'Authentication failure';
+            message = "Unable to authenticate user";
         }
+
+        if(message){
+            res.status(401).json({ errorMessage: 'Access Denied'});
+        } else {
+            next();
+        }
+    } catch (error) {
+        res.json({error: error.message});
     }
-    
-    if(message){
-        res.status(401).json({ errorMessage: 'Access Denied'});
-    } else {
-        next();
-    }
+
 };
